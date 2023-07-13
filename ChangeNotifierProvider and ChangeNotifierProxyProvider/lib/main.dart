fcb8b02f39e1685295b87ef7ecae4316b34651ca
Nicolas Teixeira <nicolasteixeira3856@outlook.com>
2023-07-13 14:31:47 -0300

    add app with changenotifierprovider and changenotifierproxyprovider

---
+++ ChangeNotifierProvider and ChangeNotifierProxyProvider/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_weather_app_provider/pages/home_page.dart';
+import 'package:flutter_weather_app_provider/providers/providers.dart';
+import 'package:flutter_weather_app_provider/repositories/weather_repository.dart';
+import 'package:flutter_weather_app_provider/services/weather_api_services.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<WeatherRepository>(
+          create: (context) => WeatherRepository(
+              weatherApiServices: WeatherApiServices(
+            httpClient: http.Client(),
+          )),
+        ),
+        ChangeNotifierProvider<WeatherProvider>(
+          create: (context) => WeatherProvider(
+              weatherRepository: context.read<WeatherRepository>()),
+        ),
+        ChangeNotifierProvider<TempSettingsProvider>(
+          create: (context) => TempSettingsProvider(),
+        ),
+        ChangeNotifierProxyProvider<WeatherProvider, ThemeProvider>(
+          create: (context) => ThemeProvider(),
+          update: (
+            BuildContext context,
+            WeatherProvider weatherProvider,
+            ThemeProvider? themeProvider,
+          ) =>
+              themeProvider!..update(weatherProvider),
+        ),
+      ],
+      builder: (context, _) => MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: context.watch<ThemeProvider>().state.appTheme == AppTheme.light
+            ? ThemeData.light()
+            : ThemeData.dark(),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
